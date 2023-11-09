c05aa1b7345011cd75d539390970e87a80d7d75b
AndresKorvin <andreskorvin@gmail.com>
2023-11-09 19:34:13 +0300

    DataHelper is ready

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -57,41 +57,86 @@ public static String getCardNumberWith15Digits() {
     return "4444 4444 4444 444";
 }
 //        - латиница вместо цифр в номере карты
+public static String getCardNumberWithLatinText() {
+    return "qwer tyui asdf ghjk";
+}
 //        - кириллица вместо цифр в номере карты
+public static String getCardNumberWithCyrillicText() {
+    return "йцук енгш фыва прол";
+}
 //        - пустые данные
-public static String getEmptyCardNumber() {
+public static String getEmpty() {
     return "";
 }
 //
 //        - неверный номер месяца действия карты
+public static String getInvalidMonth() { return LocalDate.now().minusDays(35).format(DateTimeFormatter.ofPattern("MM")); }
 //        - месяц больше 12
+public static String getMonthNum13() { return "13"; }
 //        - месяц 0
-//        - отрицательный месяц
+public static String getZeroMonth() { return "00"; }
 //        - латиница вместо цифр в поле месяц
+public static String getMonthWithLatinText() { return LocalDate.now().format(DateTimeFormatter.ofPattern("MMM")); }
 //        - кириллица вместо цифр в поле месяц
+public static String getMonthWithCyrillicText() { return LocalDate.now().format(DateTimeFormatter.ofPattern("МММ")); }
 //        - пустые данные
 //
 //        - неверный номер года действия карты
+public static String getPastYear() { return LocalDate.now().minusYears(1).format(DateTimeFormatter.ofPattern("yy")); }
 //        - год больше текущего
-//        - отрицательный год
+public static String getFutureYear() { return LocalDate.now().plusYears(5).format(DateTimeFormatter.ofPattern("yy")); }
 //        - латиница вместо цифр в поле год
+public static String getYearWithLatinText() {
+    return "ye";
+}
 //        - кириллица вместо цифр в поле год
+public static String getYearWithCyrillicText() {
+    return "дв";
+}
 //        - пустые данные
 //
 //        - неверное заполненное латиницей поле Владелец
+public static String getnIvalidFullName() {
+    return fakerEn.name().fullName();
+}
 //        - кириллица вместо латиницы в поле Владелец
+public static String getCyrillicDataOwner() {
+    return "Иван Иванов";
+}
 //        - цифры вместо букв в поле Владелец
+public static String getOwnerWithDigits() {
+    return fakerEn.number().digits(10);
+}
 //        - только имя в поле владелец
+public static String getOnlyNameOwner() {
+    return "Ivan";
+}
 //        - фамилия, имя + излишнее отчество
-//        - имя фамилия на двух языках
+public static String getFullName() {
+    return "Ivan Ivanovich Ivanov";
+}
 //        - спецсимволы в поле владелец
+public static String getOwnerWithSpecialChars() {
+    return "!№%?*";
+}
 //        - пустые данные
 //
-//        - 1 цифра в поле CVC/CVV код
 //        - 2 цифры в поле CVC/CVV код
-//        - 4 цифры в поле CVC/CVV код
+public static String getInvalidFormatCodeWhith2Digits() {
+    return fakerEn.number().digits(2);
+}
 //        - латиница вместо цифр в поле CVC/CVV код
+public static String getCodeWithLatinText() {
+    return "cod";
+}
 //        - кириллица вместо цифр в поле CVC/CVV код
+public static String getCodeWithCyrillicText() {
+    return "код";
+}
+//        - спецсимволы в поле CVC/CVV код
+public static String getCvcWithSpecialChars() {
+    return "!99";
+}
 //        - пустые данные
 
 
